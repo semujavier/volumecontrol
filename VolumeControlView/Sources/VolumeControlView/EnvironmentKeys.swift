7c55c360c71dd30b09e1307f5424c502076d7694
Javier Serrano <javierserrano@outlook.com>
2024-11-07 13:38:06 +0100

    Simplify environment values by using Entry macro

--- VolumeControlView/Sources/VolumeControlView/EnvironmentKeys.swift
+++ VolumeControlView/Sources/VolumeControlView/EnvironmentKeys.swift
@@ -6,22 +6,8 @@
 //
 import SwiftUI
 
-private struct FilledColorKey: EnvironmentKey {
-    static let defaultValue: Color = .accentColor
-}
-
-private struct UnfilledColorKey: EnvironmentKey {
-    static let defaultValue: Color = .secondary
-}
-
 extension EnvironmentValues {
-    var filledColor: Color {
-        get { self[FilledColorKey.self] }
-        set { self[FilledColorKey.self] = newValue }
-    }
+    @Entry var filledColor: Color = .accentColor
+    @Entry var defaultValue: Color = .secondary
     
-    var unfilledColor: Color {
-        get { self[UnfilledColorKey.self] }
-        set { self[UnfilledColorKey.self] = newValue }
-    }
 }
