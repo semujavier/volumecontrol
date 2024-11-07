ae8329da929bac5a9a47bd601a2d19e3e5231b9d
Javier Serrano <javierserrano@outlook.com>
2024-11-07 11:51:16 +0100

    Initial Commit

---
+++ VolumeControl/VolumeControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeControlApp.swift
+//  VolumeControl
+//
+//  Created by Javier Serrano Muñoz on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct VolumeControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
